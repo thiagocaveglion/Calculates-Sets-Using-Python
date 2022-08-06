489e8cf8e0b2591d338ef57b8989ca173343bc4a
Thiago Rossatto Caveglion <thiagocaveglion@hotmail.com>
2022-08-06 14:59:22 -0300

    Update program.py

--- program.py
+++ program.py
@@ -8,35 +8,41 @@ i = list()
 d = list()
 c = list()
 s = None
+o = 0
 num = "algumas"
 # u = uniao
 # i = interseccao
 # d = diferenca
 # c = produto cartesiano
 for char in data.split("\n"):
-    if char.strip() != "":
-        lista.append(char)
+    lista.append(char.strip())
 
-for linha in lista:
+for indice, linha in enumerate(lista):
     if linha != "":
         if linha == lista[0] and linha in "0123456789":
             num = linha[0]
             continue
-        if len(linha) == 1 and linha[0] in "UIDC":
+        elif len(linha) == 1 and linha.strip() in "UIDC":
             s = 2
-            operacao = linha[0]
+            operacao = linha.strip()
+            continue
         else:
-            s -= 1
+            try: s -= 1
+            except: print(f"o termo {linha.strip()} é inválido, remova-o do arquivo txt na linha {indice+1}")
+          
         if (s == 0 or s == 1):
             lista2.append(linha.split(","))
             if s == 0:
+              
                 if operacao == "U":
+                    o += 1
                     lista3 = lista2[0][:] + lista2[1][:]
                     lista2.clear()
                     u.append(lista3[:])
                     lista3.clear()
 
                 elif operacao == "I":
+                    o += 1
                     for inteiro in lista2[0]:
                         if inteiro in lista2[1]:
                             lista3.append(inteiro)
@@ -45,6 +51,7 @@ for linha in lista:
                     lista3.clear()
 
                 elif operacao == "D":
+                    o += 1
                     for inteiro in lista2[0]:
                         if inteiro not in lista2[1]:
                             lista3.append(inteiro)
@@ -53,6 +60,7 @@ for linha in lista:
                     lista3.clear()
 
                 elif operacao == "C":
+                    o += 1
                     for char in lista2[0]:
                         for char2 in lista2[1]:
                             if char in "0123456789" and char2 in "0123456789":
@@ -63,24 +71,30 @@ for linha in lista:
                     c.append(lista3[:])
                     lista3.clear()
 
-print(f"serão realizadas {num} operações")
+if "" in lista: print("tente evitar os espaços em branco no arquivo txt")
+print("-"*60)
+if num != "algumas": print(f"foi informada a realização de {num} operações")
+else: print(f"{num} operações serão realizadas")
 print("operações de união geraram a(s) lista(s): ")
-print("-"*58)
+print("-"*60)
 for j in u: 
-  print(j)
-  print("-"*58)
+    print(j)
+    print("-"*60)
 print("operações de intersecção geraram a(s) lista(s): ")
-print("-"*58)
+print("-"*60)
 for j in i:
-  print(j)
-  print("-"*58)
+    print(j)
+    print("-"*60)
 print("operações de diferença geraram a(s) lista(s): ")
-print("-"*58)
+print("-"*60)
 for j in d: 
-  print(j)
-  print("-"*58)
+    print(j)
+    print("-"*60)
 print("operações de produto cartesiano geraram a(s) lista(s): ")
-print("-"*58)
+print("-"*60)
 for j in c: 
-  print(j)
-  print("-"*58)
+    print(j)
+    print("-"*60)
+print(f"foram realizadas {o} operações")
+if str(o) != num: print("tente inserir o número correto de operações no txt")
+print("-"*60)
